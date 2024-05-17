ec22c263f1908adb87e9ff015aaa7a735ed1e653
ReyRodriP <reynaldorodriguez802@gmail.com>
2024-05-17 10:57:53 -0400

    El mismo pero con contenido

---
+++ Git.java
@@ -0,0 +1,8 @@
+public class Git{
+         
+    public static void main(String[] args){
+        String Message = "Esta es una prueba de git, se trata de la prueba ";
+        byte Num = 2;
+        System.out.println(Message+Num);
+    }
+}
